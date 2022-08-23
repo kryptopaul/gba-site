c35292a5431baf52e3435c94c56aea59227075dd
xisux <87016182+xisux@users.noreply.github.com>
2022-08-23 15:14:45 +0200

    testing on prod

---
+++ articles/polygon-store.md
@@ -0,0 +1,6 @@
+## This is a Polygon store test!
+
+in this guide blah blah blah
+
+- List 1
+- List 2
--- src/routes/article.tsx
+++ src/routes/article.tsx
@@ -3,20 +3,41 @@ import { Container } from "@mantine/core";
 import { Title } from "@mantine/core";
 import { FooterSimple } from "../FooterSimple";
 import { useParams } from "react-router-dom";
+import ReactMarkdown from 'react-markdown'
+import { Loader } from '@mantine/core';
+import { useEffect, useState } from "react";
 
 const headerLinks = [{link: '/', label: 'Home'}, {link: '/articles', label: 'Articles'}, {link: '/', label: 'Join us'}];
 const footerLinks: {link: string, label: string}[] = [{link: '/', label: 'Home'}, {link: '/articles', label: 'Articles'}, {link: '/', label: 'Join Us'}, {link: '/', label: 'Contact'}];
 
 
-export default function Article() {
+
+export default function Article() {   
+
     const params = useParams();
     console.log(params)
+
+    const [article, setArticle] = useState<any>();
+
+    async function loadArticle(id:any) {
+        const response = await fetch(`https://raw.githubusercontent.com/kryptopaul/gba-site/master/articles/${id}.md`);
+        const responseText = await response.text();
+        setArticle(responseText);
+    }
+
+    useEffect(() => {
+        loadArticle(params.id);
+    } , []);
+
     return (
-        <Container>   
+        <>
         <HeaderResponsive links={headerLinks} />
+        <Container>   
             <Title style={{marginBottom: '25px'}}>{"This is Article with ID: " + params.id}</Title>
-            
-        <FooterSimple {...{links: footerLinks}} />
+            <Loader color="orange" size="xl" />
+            <ReactMarkdown children={article}></ReactMarkdown>
         </Container>     
+        <FooterSimple {...{links: footerLinks}} />
+        </>
     );
   }
