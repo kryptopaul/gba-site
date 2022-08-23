d00b90413a1b4d341d3f37d6d0b940d3dcccf302
xisux <87016182+xisux@users.noreply.github.com>
2022-08-23 17:54:00 +0200

    fixing articles

--- src/routes/articles.tsx
+++ src/routes/articles.tsx
@@ -4,6 +4,7 @@ import { Title } from "@mantine/core";
 import { Grid } from "@mantine/core";
 import { NewsCard } from "../NewsCard";
 import { FooterSimple } from "../FooterSimple";
+import { Link } from "react-router-dom";
 
 const headerLinks = [{link: '/', label: 'Home'}, {link: '/articles', label: 'Articles'}, {link: '/', label: 'Join us'}];
 const footerLinks: {link: string, label: string}[] = [{link: '/', label: 'Home'}, {link: '/articles', label: 'Articles'}, {link: '/', label: 'Join Us'}, {link: '/', label: 'Contact'}];
@@ -25,8 +26,10 @@ export default function Articles() {
           <NewsCard {...{title: 'Greenwich Blockchain Association sign-ups are open!', category: 'Ethereum', image: 'https://bitcoin.pl/wp-content/uploads/2022/03/ethereum-pos.jpg', link: 'https://example.com', author: {name: 'Paul Lechocki', image: 'https://i.imgur.com/MlliW1v.png', description:'Founder'}}} />
         </Grid.Col>
 
-          <Grid.Col md={6} lg={4}>
-            <NewsCard {...{title: 'Building a transparent merch store with Polygon', category: 'polygon', image: 'https://i.imgur.com/CJNRmnv.png', link: 'https://example.com', author: {name: 'Paul Lechocki', image: 'https://i.imgur.com/MlliW1v.png', description:'Founder'}}} />
+        <Grid.Col md={6} lg={4}>
+            <Link to='/article/polygon-store' style={{textDecoration: 'none'}}>
+            <NewsCard {...{title: 'Building a transparent merch store with Polygon', category: 'polygon', image: 'https://i.imgur.com/CJNRmnv.png', link: null, author: {name: 'Paul Lechocki', image: 'https://i.imgur.com/MlliW1v.png', description:'Founder'}}} />
+            </Link>
           </Grid.Col>
           
 
