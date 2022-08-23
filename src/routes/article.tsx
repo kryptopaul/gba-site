eeddd00f94b97fce95ee9e27e9dfd184a6e003f1
xisux <87016182+xisux@users.noreply.github.com>
2022-08-23 16:01:07 +0200

    added images to match the article id

--- src/routes/article.tsx
+++ src/routes/article.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import ReactMarkdown from 'react-markdown'
 import { Loader } from '@mantine/core';
 import { useEffect, useState } from "react";
+import { Image } from "@mantine/core";
 
 const headerLinks = [{link: '/', label: 'Home'}, {link: '/articles', label: 'Articles'}, {link: '/', label: 'Join us'}];
 const footerLinks: {link: string, label: string}[] = [{link: '/', label: 'Home'}, {link: '/articles', label: 'Articles'}, {link: '/', label: 'Join Us'}, {link: '/', label: 'Contact'}];
@@ -16,6 +17,7 @@ export default function Article() {
     const params = useParams();
 
     const [article, setArticle] = useState<any>();
+    const [image , setImage] = useState<any>();
     const [loading, setLoading] = useState<string>("block");
 
     useEffect(() => {
@@ -24,9 +26,14 @@ export default function Article() {
 
     async function loadArticle(id:any) {
         try{
+        // Fetch article
         const response = await fetch(`https://raw.githubusercontent.com/kryptopaul/gba-site/master/articles/${id}.md`);
         const responseText = await response.text();
+
+        // Fetch image
+        const image = `https://raw.githubusercontent.com/kryptopaul/gba-site/master/articles/${id}.png`;
         setLoading("none");
+        setImage(image);
         setArticle(responseText);
         } catch (error) {
             console.log(error)
@@ -40,6 +47,7 @@ export default function Article() {
         <HeaderResponsive links={headerLinks} />
         <Container>   
             <Loader style={{display: loading}} color="orange" size="xl" />
+            <Image src={image}  />
             <ReactMarkdown children={article}></ReactMarkdown>
         </Container>     
         <FooterSimple {...{links: footerLinks}} />
